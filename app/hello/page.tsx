c19ce7e669b20d15dfe16a312a41d42146688f8a
Minae <ydmaad@gmail.com>
2025-11-19 17:52:48 +0900

    feat: Next.js 가이드라인 추가 및 페이지 컴포넌트 생성

---
+++ app/hello/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-8">
+      <p>here is hello</p>
+    </main>
+  );
+};
+
+export default Page;
---
+++ app/wifi/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <main className="min-h-screen flex items-center justify-center p-8">
+      <h1>Hello</h1>
+    </main>
+  );
+};
+
+export default Page;
